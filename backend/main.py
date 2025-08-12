db1bb3c3fa956142e6bf9513dfd131749ce4b3ec
InnocentZwelo <zithazweloj@gmail.com>
2025-08-12 18:36:59 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,11 +21,12 @@ app = FastAPI()
 frontend_origin = os.getenv("FRONTEND_ORIGIN", "*")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all for testing
+    allow_origins=[frontend_origin],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
+    allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # --- Pydantic Model for API Input ---
 class FarmDataRequest(BaseModel):
